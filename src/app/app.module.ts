6d9becca513f4f6ebae7b73313a66afb2a140c7e
yashchandel0007 <yashchandel007@outlook.com>
2022-05-09 00:28:10 +0530

    initial commit: project added, material module added, mat-toolbar added

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FilterpageComponent } from './filterpage/filterpage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FilterpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatCardModule} from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatButtonModule } from '@angular/material/button'
+import {MatTabsModule  } from '@angular/material/tabs'
+import { MatListModule } from '@angular/material/list';
+import {MatProgressBarModule} from '@angular/material/progress-bar'
+
+const material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatInputModule,
+  MatCardModule,
+  MatSnackBarModule,
+  MatButtonModule,
+  MatTabsModule,
+  MatListModule,
+  MatProgressBarModule
+];
+
+@NgModule({
+  imports: [
+    material
+  ],
+  exports: [
+    material
+  ]
+})
+
+export class MaterialModule { }
